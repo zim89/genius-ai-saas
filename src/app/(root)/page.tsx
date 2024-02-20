d3b3a814dcf9d88a06664b915b992eb2c2cbc208
zim89 <121241841+zim89@users.noreply.github.com>
2024-02-20 13:13:18 +0200

    add Clerk auth

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 const HomePage = () => {
+  const isSignedIn = true;
   return (
     <div>
-      <h2>Home Page</h2>
+      <h2>Home Page (unprotected)</h2>
+      <Button variant='outline' className='rounded-full' asChild>
+        <Link href={isSignedIn ? '/dashboard' : '/login'}>Get Started</Link>
+      </Button>
     </div>
   );
 };
