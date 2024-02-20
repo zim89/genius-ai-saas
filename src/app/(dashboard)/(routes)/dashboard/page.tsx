8951f393046f84a8189d4df3cd823cf9e670cceb
zim89 <121241841+zim89@users.noreply.github.com>
2024-02-20 15:31:50 +0200

    add Dashboard page, fixed hydration errors

--- src/app/(dashboard)/(routes)/dashboard/page.tsx
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,40 @@
+'use client';
+import { Card } from '@/components/ui/card';
+import { useRouter } from 'next/navigation';
+import { ArrowRight } from 'lucide-react';
+import { tools } from '@/lib/constants';
+import { cn } from '@/lib/utils';
+
 const Page = () => {
+  const router = useRouter();
+
   return (
     <div>
-      <h2>Dashboard Page (Protected)</h2>
+      <div className='mb-8 space-y-4'>
+        <h2 className='text-center text-2xl font-bold md:text-4xl'>
+          Explore the power of AI
+        </h2>
+        <p className='text-center text-sm font-light text-muted-foreground md:text-lg'>
+          Chat with the smartest AI - Experience the power of AI
+        </p>
+      </div>
+
+      <div className='space-y-4 px-4 md:px-20 lg:px-32'>
+        {tools.map((tool) => (
+          <Card
+            onClick={() => router.push(tool.href)}
+            key={tool.href}
+            className='flex cursor-pointer items-center justify-between border-black/5 p-4 transition hover:shadow-md'>
+            <div className='flex items-center gap-x-4'>
+              <div className={cn('w-fit rounded-md p-2', tool.bgColor)}>
+                <tool.icon className={cn('h-8 w-8', tool.color)} />
+              </div>
+              <div className='font-semibold'>{tool.label}</div>
+            </div>
+            <ArrowRight className='h-5 w-5' />
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
