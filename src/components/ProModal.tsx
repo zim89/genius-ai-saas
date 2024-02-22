8044ddec97e0a9eea9a38029d36dd2b3143bd172
zim89 <121241841+zim89@users.noreply.github.com>
2024-02-22 17:03:23 +0200

    add stripe payment system

--- src/components/ProModal.tsx
+++ src/components/ProModal.tsx
@@ -1,11 +1,12 @@
 'use client';
+import axios from 'axios';
+import { useState } from 'react';
 import { Check, Zap } from 'lucide-react';
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle,
-  DialogDescription,
   DialogFooter,
 } from '@/components/ui/dialog';
 import { Badge } from '@/components/ui/badge';
@@ -17,6 +18,19 @@ import { cn } from '@/lib/utils';
 
 const ProModal = () => {
   const proModal = useProModal();
+  const [loading, setLoading] = useState(false);
+
+  const onSubscribe = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get('/api/stripe');
+      window.location.href = response.data.url;
+    } catch (error) {
+      console.log(error, 'STRIPE_CLIENT_ERROR');
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -32,7 +46,6 @@ const ProModal = () => {
               </Badge>
             </div>
           </DialogTitle>
-          {/*<DialogDescription className='space-y-2 pt-2 text-center font-medium text-zinc-900'></DialogDescription>*/}
           <div className='space-y-2 pt-2 text-center font-medium text-zinc-900'>
             {tools.map((tool) => (
               <Card
@@ -50,7 +63,11 @@ const ProModal = () => {
           </div>
         </DialogHeader>
         <DialogFooter>
-          <Button size='lg' variant='premium' className='w-full'>
+          <Button
+            size='lg'
+            variant='premium'
+            onClick={onSubscribe}
+            className='w-full'>
             Upgrade
             <Zap className='ml-2 h-4 w-4 fill-white' />
           </Button>
