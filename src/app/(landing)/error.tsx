3ce2855003f848d0d9558fbf02e753d3e901ef50
zim89 <121241841+zim89@users.noreply.github.com>
2024-02-29 19:59:19 +0200

    add Landing page

---
+++ src/app/(landing)/error.tsx
@@ -0,0 +1,8 @@
+'use client';
+import Empty from '@/components/Empty';
+
+const Error = () => {
+  return <Empty label='Something went wrong.' />;
+};
+
+export default Error;
---
+++ src/components/LandingHero.tsx
@@ -0,0 +1,48 @@
+'use client';
+import TypewriterComponent from 'typewriter-effect';
+import Link from 'next/link';
+import { useAuth } from '@clerk/nextjs';
+import { Button } from '@/components/ui/button';
+
+const LandingHero = () => {
+  const { isSignedIn } = useAuth();
+
+  return (
+    <div className='space-y-5 py-36 text-center font-bold text-white'>
+      <div className='space-y-5 text-4xl font-extrabold sm:text-5xl md:text-6xl lg:text-7xl'>
+        <h1>The Best AI Tool for</h1>
+        <div className='h-20 bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-transparent'>
+          <TypewriterComponent
+            options={{
+              strings: [
+                'Chatbot.',
+                'Photo Generation.',
+                'Blog Writing.',
+                'Mail Writing.',
+              ],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </div>
+      </div>
+      <div className='text-sm font-light text-zinc-400 md:text-xl'>
+        Create content using AI 10x faster.
+      </div>
+      <div>
+        <Link href={isSignedIn ? '/dashboard' : '/sign-up'}>
+          <Button
+            variant='premium'
+            className='rounded-full p-4 font-semibold md:p-6 md:text-lg'>
+            Start Generating For Free
+          </Button>
+        </Link>
+      </div>
+      <div className='text-xs font-normal text-zinc-400 md:text-sm'>
+        No credit card required.
+      </div>
+    </div>
+  );
+};
+
+export default LandingHero;
